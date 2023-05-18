5ea5899c97cf87eace5d02c487c08c076464b83e
123qq1 <jeppe.bjerremand@gmail.com>
2023-05-18 17:17:14 +0200

    Began convoluting input to state libary.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use image::GenericImageView;
+use image::io::Reader as ImageReader;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, wave function collapse!");
+
+    let img = ImageReader::open("img/Cave.png").unwrap().decode().unwrap();
+
+    let mut library = Vec::new();
+
+    for pixel in img.pixels(){
+        if pixel.0 == 0 || pixel.1 == 0 {continue;}
+        if pixel.0 == 15 || pixel.1 == 15 {continue;}
+
+        let x = pixel.0;
+        let y = pixel.1;
+
+        let state = [
+            img.get_pixel(x-1,y-1),img.get_pixel(x,y-1),img.get_pixel(x+1,y-1),
+            img.get_pixel(x-1,y),pixel,img.get_pixel(x+1,y),
+            img.get_pixel(x-1,y+1),img.get_pixel(x,y+1),img.get_pixel(x+1,y+1)
+        ];
+
+        library.push(state);
+
+        print!("pixel {} {}, ",pixel.0,pixel.1);
+    }
 }
